ac533bd25a75897c36c7c2e01aa57ed007b29955
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-29 17:42:10 -0400

    feat(Product--be): update products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const productSchema = require('../models/productModel');
 
-//*Create Products
+//*Create Products --Admin
 exports.createProduct = async (req, res, next) => {
   const product = new productSchema(req.body);
 
@@ -8,7 +8,8 @@ exports.createProduct = async (req, res, next) => {
     .save()
     .then((data) => {
       res.json({
-        message: 'success',
+        message: 'createProduct --route working fine',
+        // data:document just created
         data,
       });
     })
@@ -20,8 +21,32 @@ exports.createProduct = async (req, res, next) => {
     });
 };
 
-exports.getAllProducts = (req, res) => {
+//?get all products
+exports.getAllProducts = async (req, res) => {
+  const allProducts = await productSchema.find();
   res.status(200).json({
-    message: 'route is working fine',
+    message: 'getAllProduct --route working fine',
+    // allproducts:all document just fetched
+    allProducts,
+  });
+};
+
+//?update product --admin
+exports.updateProduct = async (req, res) => {
+  let product = await productSchema.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: 'product not found',
+    });
+  }
+  product = await productSchema.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
   });
 };
