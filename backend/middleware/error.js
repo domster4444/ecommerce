4fc2b9482b041c64ca37a365b22c46d7f94a596d
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-30 09:29:51 -0400

    (err Handling): mongodb err, uncaught err COMPLETE MAJOR ERROR HANDLING

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,8 +2,15 @@ const ErrorHandler = require('../utils/errorhandler');
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Internal Server Error';
+
+  //Wrong mongodb id error
+  if (err.name === 'CastError') {
+    const message = `Resource not found.Invalid : ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
-    error: err.stack,
+    error: err.message,
   });
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const connectToAtlas = require('./config/database');
+
+//handling uncaught exception error
+process.on('uncaughtException', (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log('Shutting down server due to uncaught exception error');
+
+  process.exit(1);
+});
+
 //*config
 dotenv.config({ path: './backend/config/config.env' });
 
@@ -11,7 +20,10 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`server is working on http://localhost::${process.env.PORT}`);
 });
 
-//unhandled promise rejection
+//uncaught error
+//// console.log(youtube);
+
+//!unhandled promise rejection
 process.on('unhandledRejection', (err) => {
   console.log(`err: ${err.message}`);
   console.log('sutting donw th server due to undhandled promise rejection');
