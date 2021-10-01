3a3eea5447334d764a96bdcad8e2e273cfb75295
Kshitiz <kshitiz.stha11@gmail.com>
2021-10-01 14:41:48 -0400

    API(updateUserProfileData): complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -180,3 +180,23 @@ exports.updateUserPassword = catchAsyncErrors(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+//* updateProfileData
+exports.updateProfileData = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  //We will add cloudinary later
+  const user = await userSchema.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,6 +7,7 @@ const {
   resetPassword,
   getUserDetail,
   updateUserPassword,
+  updateProfileData,
 } = require('../controllers/userController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
@@ -23,5 +24,6 @@ router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(logoutUser);
 router.route('/me').get(isAuthenticatedUser, getUserDetail);
 router.route('/password/update').put(isAuthenticatedUser, updateUserPassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfileData);
 
 module.exports = router;
