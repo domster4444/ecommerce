642254b38281bdc3ac199b769f0929dc5d54a6ae
Kshitiz <kshitiz.stha11@gmail.com>
2021-10-01 09:18:28 -0400

    ERRORHANDLING(JWT): handle minor error case

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,9 +3,27 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Internal Server Error';
 
-  //Wrong mongodb id error
+  //!Wrong mongodb id error
   if (err.name === 'CastError') {
     const message = `Resource not found.Invalid : ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //!Mongoose duplicate key error
+  if ((err.code = 11000)) {
+    const message = `duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //!Wrong JWT error
+  if ((err.code = 'JsonWebTokenError')) {
+    const message = `Json Web Token is invalid, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //!JWT Expire error
+  if ((err.code = 'TokenExpiredError')) {
+    const message = `Json Web Token is Expired, Please Login Again`;
     err = new ErrorHandler(message, 400);
   }
 
